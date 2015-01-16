5a5e05a75e9423a422ec850758ac9eaed7821c67
vern14 <vzidana@gmail.com>
2015-01-15 22:17:52 -0800

    5.6

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ window.onresize=function(){
 document.getElementById("menu").style.display="none";
 
 document.getElementById("x").style.display="none";
+else
+var x = document.getElementsByClassName("gridmenu");
+    x[0].style.display="none";
+document.getElementById("menu").style.display="block";
+
+document.getElementById("x").style.display="none";
 }
 
 }
