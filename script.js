b03f8dcfc7120d73bcb07d14e830a6bb43049026
vern14 <vzidana@gmail.com>
2015-01-15 22:12:34 -0800

    5.5

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ window.onresize=function(){
 	if(window.innerWidth>1000)
     document.getElementById("menu").reload();
 	var x = document.getElementsByClassName("gridmenu");
-    x[0].style.display="none";
-document.getElementById("menu").style.display="block";
+    x[0].style.display="block";
+document.getElementById("menu").style.display="none";
 
 document.getElementById("x").style.display="none";
 }
