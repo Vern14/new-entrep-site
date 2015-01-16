da519347519b3fb3163ab630396d43297460210d
vern14 <vzidana@gmail.com>
2015-01-15 23:33:13 -0800

    6.4

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-onload=function (){
 
 document.getElementById("menu").onclick=function (){
 var x = document.getElementsByClassName("gridmenu");
@@ -13,19 +12,23 @@ document.getElementById("menu").style.display="none";
 document.getElementById("x").onclick=function v(){
 var x = document.getElementsByClassName("gridmenu");
     x[0].style.display="none";
+	document.getElementById("x").style.display="none";
 document.getElementById("menu").style.display="block";
 
-document.getElementById("x").style.display="none";
 
+location.reload();
 }
+
+
 
 window.onresize=function(){
 	if(window.innerWidth>1000)
-  
-	document.getElementsByClassName("gridmenu").reload();
 
+  	document.getElementById("x").style.display="none";
+	
+var x = document.getElementsByClassName("gridmenu");
+    x[2].style.display="none";
 
 
 }
- 
-}
+
