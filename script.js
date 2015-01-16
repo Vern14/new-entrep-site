232150e65c4f25460eefcfaf6aedaa00264d31d0
vern14 <vzidana@gmail.com>
2015-01-15 22:07:35 -0800

    5.2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-onload=function(){
+onload=function (){
 
 document.getElementById("menu").onclick=function (){
 var x = document.getElementsByClassName("gridmenu");
@@ -10,7 +10,7 @@ document.getElementById("menu").style.display="none";
 	
 
 }
-document.getElementById("x").onclick=function (){
+document.getElementById("x").onclick=function v(){
 var x = document.getElementsByClassName("gridmenu");
     x[0].style.display="none";
 document.getElementById("menu").style.display="block";
@@ -22,7 +22,7 @@ document.getElementById("x").style.display="none";
 window.onresize=function(){
 	if(window.innerWidth>1000)
     document.getElementById("menu").reload();
-	document.getElementById("x").style.display="none";
+	v();
 }
 
 }
