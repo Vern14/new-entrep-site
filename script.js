47763b9263bd11395f50356a973379b80114bfb0
vern14 <vzidana@gmail.com>
2015-01-15 22:19:51 -0800

    5.7

--- script.js
+++ script.js
@@ -21,18 +21,20 @@ document.getElementById("x").style.display="none";
 
 window.onresize=function(){
 	if(window.innerWidth>1000)
-    document.getElementById("menu").reload();
+  
 	var x = document.getElementsByClassName("gridmenu");
     x[0].style.display="block";
 document.getElementById("menu").style.display="none";
 
 document.getElementById("x").style.display="none";
+ document.getElementById("menu").reload();
 else
 var x = document.getElementsByClassName("gridmenu");
     x[0].style.display="none";
 document.getElementById("menu").style.display="block";
 
 document.getElementById("x").style.display="none";
+ document.getElementById("menu").reload();
 }
 
 }
